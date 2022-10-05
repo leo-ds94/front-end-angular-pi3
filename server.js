f9732d43d5d61d6b4850e6bd72016b1d068181dd
Leonardo Silva Francisco <113557373+leo-ds94@users.noreply.github.com>
2022-10-05 01:16:43 -0300

    Terceiro commit - Correção deploy heroku

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
+// importar express
 const express = require('express');
-const path = require('path');
+
+// iniciar express
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-
+// nome da pasta no dist que sera feito o build
 const appName = 'site-dpto';
 
 const outputPath = `${__dirname}/dist/${appName}`;
 
-app.use(express.json());
-
 // seta o diretorio de build para servir o conteudo Angular
 app.use(express.static(outputPath));
+
 // redirecionar qualquer requisicao para o index.html
 app.get('/*', (req, res) => {
   res.sendFile(`${outputPath}/index.html`);
 });
 // ouvir a porta que o Heroku disponibilizar
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Servidor em funcionamento na porta ' + PORT));
